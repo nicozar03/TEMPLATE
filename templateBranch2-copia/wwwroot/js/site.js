2187eb7977cbe95a543e29c49911b3c366c9654c
nicozar03 <87132426+nicozar03@users.noreply.github.com>
2022-08-26 11:47:54 -0300

    a

--- templateBranch2-copia/wwwroot/js/site.js
+++ templateBranch2-copia/wwwroot/js/site.js
@@ -1,4 +1,28 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+var a_parent =  document.querySelectorAll(".a_parent");
+var dd_menu_a = document.querySelectorAll(".dd_menu_a");
 
+a_parent.forEach(function(aitem){
+
+		aitem.addEventListener("click", function(){
+			a_parent.forEach(function(aitem){
+				aitem.classList.remove("active");
+			})
+			dd_menu_a.forEach(function(dd_menu_item){
+				dd_menu_item.classList.remove("active");
+			})
+			aitem.classList.add("active");
+		})
+})
+
+dd_menu_a.forEach(function(dd_menu_item){
+
+		dd_menu_item.addEventListener("click", function(){
+			dd_menu_a.forEach(function(dd_menu_item){
+				dd_menu_item.classList.remove("active");
+			})
+			dd_menu_item.classList.add("active");
+		})
+})
 // Write your JavaScript code.
